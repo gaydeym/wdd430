2c5483c2e9e7aded45721ab84d889dfa5d5c2e12
gaydeym <gay20001@byui.com>
2025-12-04 16:20:07 -0500

    Week 07

--- src/app/documents/document-list/document-list.ts
+++ src/app/documents/document-list/document-list.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Document } from '../document.model';
 import { DocumentsService } from '../documents.service';
 
@@ -8,16 +9,24 @@ import { DocumentsService } from '../documents.service';
   templateUrl: './document-list.html',
   styleUrl: './document-list.css',
 })
-export class DocumentList implements OnInit {
+export class DocumentList implements OnInit, OnDestroy {
   documents: Document[] = [];
   documentId: string = '';
+  private docChangeSub!: Subscription;
 
   constructor(private documentsService: DocumentsService) {}
 
   ngOnInit() {
     this.documents = this.documentsService.getDocuments();
-    this.documentsService.documentChangedEvent.subscribe((documents: Document[]) => {
-      this.documents = documents;
-    });
+
+    this.docChangeSub = this.documentsService.documentListChangedEvent.subscribe(
+      (documentsList: Document[]) => {
+        this.documents = documentsList;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.docChangeSub.unsubscribe();
   }
 }
